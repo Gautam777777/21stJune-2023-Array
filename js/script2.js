e032cd2ab2ce5d6e91b326ac119aa08ba91310c6
gautam777777 <gautamsharma0057@gmail.com>
2023-06-21 09:14:17 +0530

    array

---
+++ js/script2.js
@@ -0,0 +1,9 @@
+
+
+//1.Function Defination Area
+function getMyFullName(...elem){  //Formal Argument
+    console.log(elem)
+}
+
+//1.Function Calling/Invoking Area
+getMyFullName("Gautam",'Sharma',19,3.5,["Virat","Kohli"]);  //Actual Argument
